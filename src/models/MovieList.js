520a00961adca20c10f61aa6d609338441fafbdc
luiza <luizaschettinii@gmail.com>
2025-02-04 19:25:59 -0300

    finish models

---
+++ src/models/MovieList.js
@@ -0,0 +1,36 @@
+class MoviesList {
+    constructor() {
+        this.movies = [];
+    }
+    addMovie(movie) {
+        this.movies.push(movie);
+    }
+    getAllMovies() {
+        return this.movies;
+    }
+
+    getMovieById(id) {
+        const movie = this.movies.find((movie) => movie.id == id);
+        if (!movie) {
+            throw new Error("Filme não encontrado");
+        }
+        return movie;
+    }
+
+    updateMovie(id, updateData) {
+        const movie = getMovieById(id);
+        Object.assign(movie, updateData);
+        return movie;
+    }
+
+    deleteMovie(id) {
+        this.movies = this.movies.filter((movie) => movie.id != id);
+    }
+
+    getTotalMovies() {
+        return this.movies.length;
+    }
+
+}
+
+module.exports = MovieList;
